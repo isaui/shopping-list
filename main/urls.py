e2190bc7e183b15ee009b44351cc084d506e2f37
isaui <isadestroyed17@gmail.com>
2023-09-20 14:00:02 +0700

    hore selesai

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
-
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id , register, login_user
+from main.views import logout_user
 app_name = 'main'
 
 urlpatterns = [
@@ -10,5 +10,8 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -6,21 +6,65 @@ from django.forms import ModelForm
 from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
 
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    product = Product.objects.all()
+    products = Product.objects.filter(user=request.user)
 
     context = {
-        'name': 'Isa Citra Buana',
+        'name': request.user.username,
         'class': 'PBP D',
-        'products':product
+        'products':products,
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "main.html", context)
 def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
+        product = form.save(commit=False)
+        product.user = request.user
         form.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
